d95918eaae4dd5f32f21f237093cec94e42c4e38
lvmin <lyuminzhang@outlook.com>
2023-03-19 14:08:10 -0700

    i

--- clean.py
+++ clean.py
@@ -4,12 +4,12 @@ import cv2
 from cv2.ximgproc import guidedFilter
 
 img = cv2.imread('input.png').astype(np.float32)
-
 y = img.copy()
+
 for _ in range(64):
     y = cv2.bilateralFilter(y, 5, 8, 8)
+
 for _ in range(4):
     y = guidedFilter(img, y, 5, 16)
 
-img = y.clip(0, 255).astype(np.uint8)
-cv2.imwrite('output.png', img)
+cv2.imwrite('output.png', y.clip(0, 255).astype(np.uint8))
