04e93a8b2ca946a9bbff6952452cb9ec9543ed39
lvmin <lyuminzhang@outlook.com>
2023-03-19 14:10:11 -0700

    i

--- clean.py
+++ clean.py
@@ -10,6 +10,6 @@ for _ in range(64):
     y = cv2.bilateralFilter(y, 5, 8, 8)
 
 for _ in range(4):
-    y = guidedFilter(img, y, 5, 16)
+    y = guidedFilter(img, y, 4, 16)
 
 cv2.imwrite('output.png', y.clip(0, 255).astype(np.uint8))
